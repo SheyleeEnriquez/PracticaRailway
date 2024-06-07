bc94dfb691b5b1b57cd2a4718884a0e34f0dfc4c
Sheylee Arielle Enriquez Hernandez <sheylee.ariel@hotmail.com>
2024-06-07 07:24:35 -0500

    Railway V4.0

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template_string('''
-        <form action="/square" method="post">
-            <label for="number">Ingrese un número:</label>
-            <input type="text" id="number" name="number">
-            <input type="submit" value="Enviar">
-        </form>
-    ''')
+    return render_template('index.html')
 
 @app.route('/square', methods=['POST'])
 def square():
-    number_str = request.form['number']
-    if not number_str.isdigit():
-        return render_template_string('''
-            <p>Por favor, ingrese un número válido.</p>
-            <a href="/">Intentar de nuevo</a>
-        ''')
-    
-    number = int(number_str)
+    number = int(request.form['number'])
     result = number ** 2
-    return render_template_string('''
-        <p>El cuadrado de {{ number }} es {{ result }}.</p>
-        <a href="/">Probar otro número</a>
-    ''', number=number, result=result)
+    return render_template('result.html', number=number, result=result)
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", default=5000))
