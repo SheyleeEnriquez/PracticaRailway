cc6ead248cc4b76d090975a5cded2121a63d40d2
Sheylee Arielle Enriquez Hernandez <sheylee.ariel@hotmail.com>
2024-06-07 01:13:07 -0500

    Railway V2.0

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template_string
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_set = {"Hello World from Railway whit Flask!"}
-    return jsonify(list(my_set))
+    return render_template_string('''
+        <form action="/square" method="post">
+            <label for="number">Ingrese un número:</label>
+            <input type="text" id="number" name="number">
+            <input type="submit" value="Enviar">
+        </form>
+    ''')
 
+@app.route('/square', methods=['POST'])
+def square():
+    number = int(request.form['number'])
+    result = number ** 2
+    return render_template_string('''
+        <p>El cuadrado de {{ number }} es {{ result }}.</p>
+        <a href="/">Probar otro número</a>
+    ''', number=number, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    port = int(os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
